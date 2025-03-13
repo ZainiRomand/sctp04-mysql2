be6d99b110d696d848c70507042cb8c485c64371
Zaini Romand <zainiromand@gmail.com>
2025-03-13 15:12:24 +0000

    added user and login

--- index.js
+++ index.js
@@ -2,32 +2,38 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const pool = require('./database');
-
-// make sure this comes AFTER dotenv config
-const productsRouter = require('./routes/products');
+// router files
+const productsRouter = require('./routes/products.js')
+const userRouter = require('./routes/users');
 
 const app = express();
 
-// Middleware
+// middlewares
 app.use(express.json());
-// only works for web-site
-app.use(cors()); 
+// if off, a frontend must be on the same domain to access your api
+// backend is hosted on example.com
+// then frotend is hosted on xyz.example.com to access or example.com/xyz.html
+app.use(cors()); // enable cross origins resources sharing (--> only works for websites)
 
-app.get('/', (req, res) => {
-  res.json({ 
-    message: "Welcome to the API" 
-    });
-});
+app.get("/", (req,res)=>{
+    res.json({
+        "message":"Welcomem to the API"
+    })
+})
 
-// Routes
-// If a request which URL begin with '/api/prodcuts
-app.use('/api/products', productsRouter);
+// Register the product routers
+// if a request which URL begins with '/api/products',
+// the remainder of the URL will be sent to productsRouter
+app.use('/api/products', productsRouter)
+app.use('/api/users', userRouter);
 
-// Start the server
 // we can specify the PORT in the .env file
 // PORT => virtual port, usually meant for networking
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// IP Address => identifies a computer on the network
+// PORT => identifies which PROGRAM (aka process) is reciving or sending data
+const PORT = process.env.PORT || 3000; // default port is 3000
+// when we do deployment, we need to set the PORT to 80 or 443
+// OR different hosting services might have different requirements for PORTS
+app.listen(PORT, () =>{
+    console.log("Server is running at PORT " + PORT )
+})
